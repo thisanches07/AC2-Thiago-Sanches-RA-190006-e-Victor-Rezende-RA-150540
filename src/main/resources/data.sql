feb554cda991e014d3ff427730550698332c26e2
Thiago <thi.sanches@hotmail.com>
2021-03-26 22:47:18 -0300

    add prod profile

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_time,email_contact) VALUES ('Abril Fest','descricao do Abril Fest','Araçoiaba da Serra','2020-10-10','2020-11-11','00:00','22:22','email');
 INSERT INTO tb_event (name,description,place,start_date,end_date,start_time,end_time,email_contact) VALUES ('October Fest','descricao do October Fest','Florianopolis','2019-10-11','2020-10-15','00:00','22:22','email');
+
